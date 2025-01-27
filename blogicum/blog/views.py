c70a3e452a23a38bf085eaeeb87af83337c71aa3
Rebruk <rebruk@tochka.com>
2025-01-27 19:06:36 +0500

    test 27.01.2025 16:45 fix pindex

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -47,9 +47,11 @@ class IndexView(ListView):
     def get_queryset(self):
         return (
             super()
-            .get_queryset().filter(is_published=True,
-                    category__is_published=True,
-                    pub_date__lte=timezone.now())
+            .get_queryset().
+            filter(is_published=True,
+                        category__is_published=True,
+                        pub_date__lte=timezone.now()
+                   )
             .annotate(comment_count=Count('comments'))
             .order_by('-pub_date')
         )
